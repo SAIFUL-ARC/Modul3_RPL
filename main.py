65d6c265d409b4f83deb77a58b6d4ec232969748
Pull <saifulllllll56@gmail.com>
2026-09-15 11:53:20 +0800

    initial commit modul 3

---
+++ main.py
@@ -0,0 +1,7 @@
+print("=== KALKULATOR TERMINAL MODUL 3 ===")
+angka1 = (float(input("Masukkan angka pertama: ")))
+angka2 = (float(input("Masukkan angka kedua: ")))
+hasil = angka1 + angka2
+
+print(f"Status: setup berhasil")
+print(f"hasil penjumlahan: {hasil}")
